ff60d82cb5604631bbacbed7789b7ae21f68da9c
Emre Ayduk <emre@nitro.io>
2013-12-19 01:57:28 +0200

    Add user_login model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218234953) do
+ActiveRecord::Schema.define(version: 20131218235215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20131218234953) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "code"
+  end
+
+  create_table "user_logins", force: true do |t|
+    t.datetime "logged_in_at"
+    t.string   "ip"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
