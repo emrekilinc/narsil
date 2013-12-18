56ef452853a3cb0d883d61f90fe7e518ac91e707
Emre Ayduk <emre@nitro.io>
2013-12-19 01:33:54 +0200

    Add code to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218232320) do
+ActiveRecord::Schema.define(version: 20131218233245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131218232320) do
     t.string   "primary_color"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "code"
   end
 
 end
