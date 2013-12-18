c09e06e16c5c54b368a1db31699a8a3dcaf31d29
Emre Ayduk <emre@nitro.io>
2013-12-19 01:17:26 +0200

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Narsil::Application.config.secret_key_base = '38fe7841a206d721223fee2b6d6627a2ccb294ce8f5efc0b0e04e011be6a10c73db6fdcc32088d190abbb15ab16600c76f9f5355084702afc9923a95f702c744'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Narsil::Application.config.session_store :cookie_store, key: '_narsil_session'
