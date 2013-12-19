d9b907ec2a75c98d0c80cf819dbf54960b90a581
Emre Ayduk <emre@nitro.io>
2013-12-19 04:13:20 +0200

    Basic API design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+require 'api_constraints'
+
 Narsil::Application.routes.draw do
   get 'login' => 'session#login', as: 'login'
   post 'login' => 'session#authenticate'
+
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      get 'errors' => 'errors#index'
+    end
+  end
   
   root 'home#index'
 end
---
+++ lib/api_constraints.rb
@@ -0,0 +1,10 @@
+class ApiConstraints
+  def initialize(options)
+    @version = options[:version]
+    @default = options[:default]
+  end
+
+  def matches?(req)
+    @default || req.headers['Accept'].include?("application/vnd.2nworks.v#{@version}")
+  end
+end
